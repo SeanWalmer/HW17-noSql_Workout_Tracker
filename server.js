7c8fc8a9d23a1a86b69ba46b2d704f26cb949ad4
SeanW <sean.walmer@outlook.com>
2020-07-11 08:18:33 -0400

    moved files out of the develop folder and into the root. Also added a few folders I will need down the road and created a basic server file that pulls in the needed dependancies and launches a server. basic HTML routes to load pages created as well.

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// Connection to mongoose database
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker", { useNewUrlParser: true });
+
+// HTML Routs -------------------------------
+//HTML route to app index/homepage
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "./public/index.html"));
+});
+
+//HTML route to stats page
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/stats.html"));
+});
+
+//HTML route to exercise page
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/exercise.html"));
+});
+// ----------------------------------------------
+
+// server start
+app.listen(PORT, () => {
+    console.log("App running on port " + PORT);
+});
