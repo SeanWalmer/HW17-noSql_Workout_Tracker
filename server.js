b3dbc2679c4bfba2e918d61987c1d488a36bde50
SeanW <sean.walmer@outlook.com>
2020-07-11 21:56:31 -0400

    added heroku mlab mongo db connection to server.js

--- server.js
+++ server.js
@@ -2,19 +2,20 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const path = require("path");
-
 const PORT = process.env.PORT || 8080;
-
 const app = express();
+
 
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
 // Connection to mongoose database
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+// local connection
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+// heroku connection
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user:password1@ds031203.mlab.com:31203/heroku_xpnw3mr0", { useNewUrlParser: true });
 
 // HTML Routes -------------------------------
 require("./routes/html.js")(app);
