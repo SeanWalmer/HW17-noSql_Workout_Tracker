293b1cddb2c0dbb1599db61a8581bce0846659f8
SeanW <sean.walmer@outlook.com>
2020-07-11 21:44:55 -0400

    added the last route for /range to pull the data into the dashboard. also added some comments for readability.

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,8 @@
 const db = require("../models");
-const { update } = require("../models/workout");
+// const { update } = require("../models/workout");
 
 module.exports = function (app) {
-
+    // grabs all workouts that are then handled throughout the app depending on need
     app.get("/api/workouts", (req, res) => {
         db.Workout.find({}).then(function (workout) {
             res.json(workout);
@@ -10,13 +10,13 @@ module.exports = function (app) {
             console.log(workout);
         });
     });
-
+    // allows for app to crete a new empy workout that can then be added to later
     app.post("/api/workouts", (req, res) => {
         db.Workout.create(req.body, (error, data) => {
             res.send(data);
         });
     });
-
+    // adds to the current workout based on id
     app.put("/api/workouts/:id", (req, res) => {
         db.Workout.findOneAndUpdate({
             _id: req.params.id
@@ -30,5 +30,13 @@ module.exports = function (app) {
         }).then(updated => {
             res.json(updated);
         });
+    });
+    // grabs all stored workouts to be displayed on dashboard. Sorted for cronological order
+    app.get("/api/workouts/range", (req, res) => {
+        db.Workout.find({}).sort({ day: 1 })
+        .then(allWorkouts => {
+                res.json(allWorkouts);
+            }
+        );
     });
 };
